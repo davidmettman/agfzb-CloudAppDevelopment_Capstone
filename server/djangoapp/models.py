5a434b4b6adf023914d98c73f58d0ba1eea8f54c
davidmettman <45125725+davidmettman@users.noreply.github.com>
2022-06-11 13:30:04 -0500

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,6 +4,31 @@ from django.utils.timezone import now
 import uuid
 import json
 
+class CarDealer:
+
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
+
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
